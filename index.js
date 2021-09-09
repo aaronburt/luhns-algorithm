8599152d44dd501e0552a700227de19e35bf80c7
aaronburt <email@aaronburt.co.uk>
2021-09-09 18:29:09 +0100

    First

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * @description This class is used to verify user input
+ */
+class Verify {
+    /**
+     * @description This method uses Luhn algorithm to check whether a 16 digit is valid - this doesn't poll any external services so isn't 100%
+     * @param {string} card - Enter the 16 Digit card number to check
+     * @returns {boolean} True or False - If the algorthim is met
+     */
+    CardChecker(card){
+        if(card.length !== 16){ throw new Error('Card must have 16 digits') }
+        let check = []
+        for(let i = 0; i < card.length; i++){
+            /* Checks the 2 1 position */ 
+            if(i % 2 === 0){
+                let result = card.charAt(i) * 2;
+                /* Checks to see if the result of * 2 is a two digit */
+                if(result > 9){
+                    /* Splits the two digit into two single values and then adds them together */ 
+                    check.push(parseInt(result.toString().charAt(0)) + parseInt(result.toString().charAt(1)))
+                }  else {
+                    /* Push the value because its less then 10 */ 
+                    check.push(parseInt(result))
+                }
+            } else {
+                check.push(parseInt(card.charAt(i)));
+            }
+        }
+        /* Sums the check array */ 
+        let checked = check.reduce((accumulator, a) => { return accumulator + a; }, 0)
+        
+        /* Checks if the value ends in a zero */ 
+        return checked % 10 ? false : true;
+    }
+}
+
+module.exports = Verify;
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Luhn's Algorithm
+
+Javascript implementation of Luhn's Algorithm used for checking the validity of a Credit/Debit card 16 digit number.
+
+
+## Usage
+```javascript
+const Verifer = new Verify();
+let Check = Verifer.CardChecker('4929207313500168')
+console.log(Check)
+```
+
+## Disclaimer
+
+This doesn't poll or check any external service and only uses the algorithm to check its validity, it's very possible that a number could be used that passes this test but still isn't a valid card number.
